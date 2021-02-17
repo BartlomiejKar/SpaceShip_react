48e1dc082bdafb46fd898b33890ac3f177dbdbf4
Bartek Karelus <bartek.karelus@gmail.com>
2021-02-17 21:25:23 +0100

    use theme provider and add primarycolor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { theme } from "Views/ThemeProvider"
 import { GlobalStyle } from "./styles/GlobalStyles.js";
 import { Wrapper } from "./styles/Container.js";
 import Spaceship from "./components/Spaceship/spaceship.js";
 import Information from "./components/Information/Information";
 import Modal from "./components/Modal/Modal"
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Wrapper>
         <Spaceship />
         <Information />
         <Modal />
       </Wrapper>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/styles/Container.js
+++ src/styles/Container.js
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 
 export const Wrapper = styled.div`
-background-color: red;
+background-color: black;
+width: 100vw;
+height: 100vh;
+position: relative;
 `;
